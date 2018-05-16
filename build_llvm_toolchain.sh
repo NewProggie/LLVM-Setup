ec10ea25baacf28fb33b1ac352ed06d935afc0ed
Kai Wolf <kai.wolf@gmail.com>
2018-05-16 11:18:19 +0200

    Add mandatory INSTALL_PREFIX argument

--- build_llvm_toolchain.sh
+++ build_llvm_toolchain.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
 REPO=http://llvm.org/svn/llvm-project
-IWYU_REPO=https://github.com/include-what-you-use
-VERSION=tags/RELEASE_600/final
-VER_NUM=6.0.0 # should logically match with version number above
-INSTALL_PREFIX=/usr/local/Cellar/llvm/${VER_NUM}
+IWYU_REPO=https://github.com/include-what-you-use/include-what-you-use.git
+VER_NUM=6.0.0
+
+if [[ -z "$1" ]]; then
+  echo "No INSTALL_PREFIX argument provided"
+  exit 1
+else
+  echo "Installing into ${INSTALL_PREFIX}"
+fi
+
+VERSION=tags/RELEASE_${VER_NUM//./}/final
+INSTALL_PREFIX="$1"
 
 # Main LLVM repo
 svn co ${REPO}/llvm/${VERSION} llvm
 
 pushd llvm
-svn co ${REPO}/cfe/${VERSION}                   tools/clang             # Clang
-svn co ${REPO}/clang-tools-extra/${VERSION}     tools/clang/tools/extra # Extra Clang Tools
-svn co ${REPO}/lld/${VERSION}                   tools/lld               # LLD Linker
-svn co ${REPO}/polly/${VERSION}                 tools/polly             # Polly Loop Optimizer
-svn co ${REPO}/compiler-rt/${VERSION}           projects/compiler-rt    # Compiler-Rt (Sanitizers)
-svn co ${REPO}/openmp/${VERSION}                projects/openmp         # OpenMP
-svn co ${REPO}/libcxx/${VERSION}                projects/libcxx         # LibCXX
-svn co ${REPO}/libcxxabi/${VERSION}             projects/libcxxabi      # LibCXX ABI
+svn co ${REPO}/cfe/${VERSION}                tools/clang             # Clang
+svn co ${REPO}/clang-tools-extra/${VERSION}  tools/clang/tools/extra # Extra Clang Tools
 
-git clone ${IWYU_REPO}/include-what-you-use.git tools/clang/tools/include-what-you-use  # IWYU
+git clone ${IWYU_REPO}                       tools/clang/tools/include-what-you-use # IWYU
 echo "add_subdirectory(include-what-you-use)" >> tools/clang/tools/CMakeLists.txt
-sed -i '' "s#/Library/Developer/CommandLineTools/usr/#${INSTALL_PREFIX}/#" \
- tools/clang/tools/include-what-you-use/iwyu_driver.cc
-sed -i '' '/Applications\/Xcode.app\/Contents\/Developer\/Toolchains\//d'  \
- tools/clang/tools/include-what-you-use/iwyu_driver.cc
-sed -i '' "s#XcodeDefault.xctoolchain/usr/include/c++/v1#${INSTALL_PREFIX}/lib/clang/${VER_NUM}/include/#" \
- tools/clang/tools/include-what-you-use/iwyu_driver.cc
+
+svn co ${REPO}/lld/${VERSION}                tools/lld               # LLD Linker
+svn co ${REPO}/polly/${VERSION}              tools/polly             # Polly Loop Optimizer
+svn co ${REPO}/compiler-rt/${VERSION}        projects/compiler-rt    # Compiler-Rt (Sanitizers)
+svn co ${REPO}/openmp/${VERSION}             projects/openmp         # OpenMP
+svn co ${REPO}/libcxx/${VERSION}             projects/libcxx         # LibCXX
+svn co ${REPO}/libcxxabi/${VERSION}          projects/libcxxabi      # LibCXX ABI
+
+# Patch IWYU on macOS for not detecting system headers correctly
+if [[ $OSTYPE == darwin* ]]; then
+  sed -i '' "s#/Library/Developer/CommandLineTools/usr/#${INSTALL_PREFIX}/#" \
+    tools/clang/tools/include-what-you-use/iwyu_driver.cc
+  sed -i '' '/Applications\/Xcode.app\/Contents\/Developer\/Toolchains\//d'  \
+    tools/clang/tools/include-what-you-use/iwyu_driver.cc
+  sed -i '' "s#XcodeDefault.xctoolchain/usr/include/c++/v1#${INSTALL_PREFIX}/lib/clang/${VER_NUM}/include/#" \
+    tools/clang/tools/include-what-you-use/iwyu_driver.cc
+fi
+
 popd
 
 mkdir -p llvm/build && pushd llvm/build
